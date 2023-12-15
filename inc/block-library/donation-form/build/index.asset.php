3517ab13259244c8525f0a60ba303b0ea4ef1849
Peter Booker <mail@peterbooker.com>
2023-12-15 14:32:40 +0000

    Build assets

--- inc/block-library/donation-form/build/index.asset.php
+++ inc/block-library/donation-form/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'b7b9829cebd1bf60c038');
+<?php return array('dependencies' => array('react', 'wp-api', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'ce38d83cd2e778089037');
--- inc/extension-library/settings-page/build/index.asset.php
+++ inc/extension-library/settings-page/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-polyfill'), 'version' => '3431a003e2eb46b8528d');
+<?php return array('dependencies' => array('react', 'wp-api', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-polyfill'), 'version' => 'd30b4b6fc1e2d9fef6dc');
