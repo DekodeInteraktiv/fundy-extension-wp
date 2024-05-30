09158b74f009b8413426fe0d8c016c6f771ac597
Stian <stian.overasen@dekode.no>
2024-05-30 14:00:41 +0200

    Change form selection component to SelectControl

--- inc/block-library/donation-form/build/index.asset.php
+++ inc/block-library/donation-form/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '9978900a756db0c44bc3');
+<?php return array('dependencies' => array('react', 'wp-api', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '33b90772abd942f4c9bb');
