169d6fa16132301f7638e754c857c087a7ec3aef
Stian <stian.overasen@dekode.no>
2024-05-30 18:05:04 +0200

    Bugfix setting form id attribute

--- inc/block-library/donation-form/build/index.asset.php
+++ inc/block-library/donation-form/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'b117fd5795a57e62fb2c');
+<?php return array('dependencies' => array('react', 'wp-api', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'bf016b16cb0d55b2ef4f');
