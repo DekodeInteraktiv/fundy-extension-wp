63ff5de89ad627149cd67fd0ee48b6fa4b0a4122
Stian <stian.overasen@dekode.no>
2024-05-31 16:33:36 +0200

    Use Placeholder component (#22)

--- inc/block-library/donation-form/build/index.asset.php
+++ inc/block-library/donation-form/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'bf016b16cb0d55b2ef4f');
+<?php return array('dependencies' => array('react', 'wp-api', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '64b47ab79429efc9c52c');
