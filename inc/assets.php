f1361a4be4b7abcc58f0afa73486aa5bb8a0c7c3
Peter Booker <mail@peterbooker.com>
2024-07-18 11:19:22 +0100

    Bugfix/External Form Renderer Script

--- inc/assets.php
+++ inc/assets.php
@@ -19,5 +19,5 @@ if ( ! defined( 'ABSPATH' ) ) {
 \add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\register_scripts' );
 
 function register_scripts() {
-	\wp_register_script( 'fundy-form-script', FUNDRAISING_PLUGIN_URL . 'dist/form-renderer.js', ['react', 'react-dom'], \FUNDRAISING_VERSION, true );
+	\wp_register_script( 'fundy-form-script', 'https://fundy-storage.fra1.cdn.digitaloceanspaces.com/form-renderer.latest.js', ['react', 'react-dom'], false, true );
 }
