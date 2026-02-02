98a48d2ab196be128f6a000e60fe4a3423569f77
Peter Booker <mail@peterbooker.com>
2026-02-02 14:25:11 +0000

    Output fundyConfig

--- inc/assets.php
+++ inc/assets.php
@@ -19,6 +19,7 @@ if ( ! \defined( 'ABSPATH' ) ) {
  * Hooks
  */
 \add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\register_assets' );
+\add_action( 'wp_head', __NAMESPACE__ . '\\output_fundy_config' );
 
 function register_assets() {
 	if (
@@ -35,4 +36,34 @@ function register_assets() {
 	if ( \apply_filters( 'fundy/enqueue/form_styles', true ) ) {
 		\wp_register_style( 'fundy-form-style', "https://assets.fundy.cloud/fundy-forms.{$suffix}.css", [], null, 'all' );
 	}
+
+}
+
+/**
+ * Output FundyConfig to window.FundyConfig on every page load.
+ *
+ * This embeds configuration data for the Fundy conversion script.
+ * All values can be filtered using the 'fundy/config' and 'fundy/config/{key}' filters.
+ *
+ * @return void
+ */
+function output_fundy_config(): void {
+	$config = [
+		'coreBaseUrl'           => \apply_filters( 'fundy/config/core_base_url', \FUNDY_CORE_URL ),
+		'surgeBaseUrl'          => \apply_filters( 'fundy/config/surge_base_url', \FUNDY_SURGE_URL ),
+		'disableDataLayerEvent' => \apply_filters( 'fundy/config/disable_data_layer_event', false ),
+		'debug'                 => \apply_filters( 'fundy/config/debug', false ),
+	];
+
+	/**
+	 * Filter the entire FundyConfig array.
+	 *
+	 * @param array $config The configuration array.
+	 */
+	$config = \apply_filters( 'fundy/config', $config );
+
+	\printf(
+		'<script>window.FundyConfig = %s;</script>' . "\n",
+		\wp_json_encode( $config, \JSON_HEX_TAG | \JSON_HEX_AMP )
+	);
 }
